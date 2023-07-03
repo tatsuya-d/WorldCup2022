beff44f27d299805d2e16c5c54c46a819939a7c1
tatsuya <daidohtatsuya@Mac.local>
2023-07-03 15:33:08 +0900

    POSTの追加を設定し、新規登録をできるように実装

--- src/main/java/com/raisetech/worldCup2022/Controller/ParticipatingCountryController.java
+++ src/main/java/com/raisetech/worldCup2022/Controller/ParticipatingCountryController.java
@@ -2,11 +2,13 @@ package com.raisetech.worldCup2022.Controller;
 
 import com.raisetech.worldCup2022.Entity.ParticipatingCountry;
 import com.raisetech.worldCup2022.Mapper.ParticipatingCountryMapper;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ParticipatingCountryController {
@@ -20,5 +22,15 @@ public class ParticipatingCountryController {
     @GetMapping("/participating-country")
     public List<ParticipatingCountry> selectContinent(@RequestParam("continent") String continent) {
         return participatingCountryMapper.findByContinent(continent);
+    }
+
+    @PostMapping("/participating-country")
+    ResponseEntity<Map<String, String>> create(@RequestBody ParticipatingCountryResponse form) {
+        // 登録処理は省略
+        URI url = UriComponentsBuilder.fromUriString("http://localhost:8080")
+                .path("/participating-country")
+                .build()
+                .toUri();
+        return ResponseEntity.created(url).body(Map.of("message", "id and name and continent was successfully created"));
     }
 }
